55883ac6d5c52eec17fe18b86480ead30b2ef426
tkmagesh <tkmagesh77@gmail.com>
2015-06-11 12:16:36 +0530

    calculator factory

--- Day-01/03-CalculatorApp/calculator.js
+++ Day-01/03-CalculatorApp/calculator.js
@@ -1,20 +1,22 @@
-var result = 0;
-var calculator = {
-    add : function(x,y){
-         result += x + y;
-    },
-    subtract : function(x,y){
-         result += x - y;
-    },
-    multiply : function(x,y){
-         result += (x * y);
-    },
-    divide : function(x,y){
-         result += (x / y);
-    },
-    getResult : function(){
-        return result;
-    }
-};
-
-module.exports = calculator;
+function getCalculator(){
+    var result = 0;
+    var calculator = {
+        add : function(x,y){
+             result += x + y;
+        },
+        subtract : function(x,y){
+             result += x - y;
+        },
+        multiply : function(x,y){
+             result += (x * y);
+        },
+        divide : function(x,y){
+             result += (x / y);
+        },
+        getResult : function(){
+            return result;
+        }
+    };
+    return calculator;
+}
+module.exports = getCalculator;
