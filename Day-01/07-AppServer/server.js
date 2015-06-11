60c8ffd587327e9cbc035f1f4a634aad90fb2042
tkmagesh <tkmagesh77@gmail.com>
2015-06-11 17:18:59 +0530

    Day-1 Complete

--- Day-01/07-AppServer/server.js
+++ Day-01/07-AppServer/server.js
@@ -22,13 +22,27 @@ var server = http.createServer(function(req, res){
             res.statusCode = 404;
             res.end();
         }
-    } else if (req.url.pathname === '/calculator'){
+    } else if (req.url.pathname === '/calculator' && req.method==='GET'){
         var operation = req.query.operation,
             n1 =parseInt(req.query.n1, 10),
             n2 = parseInt(req.query.n2, 10);
         var result = calculator[operation](n1, n2);
         res.write(result.toString());
         res.end();
+    } else if (req.url.pathname === '/calculator' && req.method==='POST'){
+        var data = '';
+        req.on('data', function(chunk){
+            data += chunk;
+        });
+        req.on('end', function(){
+            var query = qs.parse(data);
+            var operation = query.operation,
+                n1 =parseInt(query.n1, 10),
+                n2 = parseInt(query.n2, 10);
+            var result = calculator[operation](n1, n2);
+            res.write(result.toString());
+            res.end();
+        });
     } else {
         res.statusCode = 404;
         res.end();
